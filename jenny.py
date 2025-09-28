4f6658716e963bfd39f1487aed5c06abdbfea884
Anwender <kokoloko.gaming@gmail.com>
2025-09-28 16:04:00 +0200

    first

---
+++ jenny.py
@@ -0,0 +1,52 @@
+# Convoluted neural network
+import torch
+import torch.nn as nn
+import torch.nn.functional as f
+import torchvision
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+num_epochs = 10
+batch_size = 32
+learing_rate = 0.001
+
+# normalisieren der reichweite
+transform = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+])
+
+train_dataset = torchvision.datasets.CIFAR10(root='./data',
+                                          train=True,
+                                          transform=transform,
+                                          download=True)
+
+test_dataset = torchvision.datasets.CIFAR10(root='./data',
+                                          train=False,
+                                          transform=transform,
+                                          download=True)
+
+# data loaders
+train_loader = torch.utils.data.DataLoader(dataset=train_dataset,
+                                           batch_size=batch_size,
+                                           shuffle=True)
+
+test_loader = torch.utils.data.DataLoader(dataset=test_dataset,
+                                          batch_size=batch_size,
+                                          shuffle=False)
+
+classes =('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+def imshow(imgs):
+    imgs = imgs / 2 + 0.5
+    npimgs = imgs.numpy()
+    plt.imshow(np.transpose(npimgs, (1, 2, 0)))
+    plt.show()
+
+dataiter = iter(train_loader)
+images, labels = next(dataiter)
+img_grid = torchvision.utils.make_grid(images[0:25], nrow=5)
+imshow(img_grid)
